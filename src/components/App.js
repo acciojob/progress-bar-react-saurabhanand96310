40bd8b639fd13cca10ce8a4d8f63efb5004f839c
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2025-04-22 03:23:36 +0000

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [update,setUpdate]=useState(0)
+  
+  useEffect(()=>{
+    const interval=setInterval(()=>{
+      setUpdate((prev)=>{
+        if(prev>=100){
+          clearInterval(interval);
+          return 0;
+        }else{
+         return prev+10
+        }
+      })
+      
+      
+    },2000)
+    return ()=> clearInterval(interval)
+  },[])
+  
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div id="barOuter">
+          <div id="barInner" style={{width:`${update}%`}}></div>
+        </div>
+        <div>{update}</div>
     </div>
   )
 }
